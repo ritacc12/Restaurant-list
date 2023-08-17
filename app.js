fb79f1c5fcf68b3238704d04e85a8edc4b39159c
ritacc12 <rita20920@gmail.com>
2023-08-17 16:20:40 +0800

    bulid:route & controller setup

--- app.js
+++ app.js
@@ -3,7 +3,16 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
+  res.redirect('/restaurant')
+})
+
+app.get('/restaurant', (req, res) => {
   res.send('express app for restaurant')
+})
+
+app.get('/restaurant/:id', (req, res) =>{
+  const id = req.params.id
+  res.send(`read restaurant:${id}`)
 })
 
 app.listen(port, () => {
