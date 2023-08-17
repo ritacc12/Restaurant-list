f26ba2b874130f089391b82e7770899aa958ee5d
ritacc12 <rita20920@gmail.com>
2023-08-17 16:08:12 +0800

    build: project init

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('express app for movies')
+  res.send('express app for restaurant')
 })
 
 app.listen(port, () => {
